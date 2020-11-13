3a8659399f27fed4dac73680e2e6b42e8155a34c
Martin <mainactivity2020@gmail.com>
2020-11-13 12:59:33 +0200

    Innit Project ExchangeRates

---
+++ app/src/main/java/com/example/exchangerates/application/ExchangeRatesApplication.kt
@@ -0,0 +1,7 @@
+package com.example.exchangerates.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExchangeRatesApplication:Application()
