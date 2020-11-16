852e596417ed551147e9a09eb7b4cdad7118bfb1
Sergio Leech <mainactivity2020@gmail.com>
2020-11-16 20:55:41 +0200

    Update ExchangeRatesFragment.kt

--- app/src/main/java/com/example/exchangerates/ui/ExchangeRatesFragment.kt
+++ app/src/main/java/com/example/exchangerates/ui/ExchangeRatesFragment.kt
@@ -53,9 +53,6 @@ class ExchangeRatesFragment : Fragment() {
                         index = _index
                         recyclerViewNBU.smoothScrollToPosition(_index)
                         Handler(Looper.getMainLooper()).postDelayed({
-                            recyclerViewNBU.findViewHolderForAdapterPosition(_index)?.itemView?.setBackgroundResource(
-                                R.color.teal_200
-                            )
                             val visible =recyclerViewNBU.findViewHolderForAdapterPosition(_index)?.itemView?.visibility
                             if (visible == 0){
                                 recyclerViewNBU.findViewHolderForAdapterPosition(_index)?.itemView?.setBackgroundResource(
